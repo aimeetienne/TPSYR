8fd9bfffceb101b5c01aa4fac496c6cda95d76e6
13007788 <13007788@e107m08.istic.univ-rennes1.fr>
2014-01-22 15:28:26 +0100

    commit  de bas

--- tpmaven/src/main/java/fr/istic/master1/sir/tpmaven/App.java
+++ tpmaven/src/main/java/fr/istic/master1/sir/tpmaven/App.java
@@ -1,12 +1,19 @@
 package fr.istic.master1.sir.tpmaven;
+
 /**
  * Hello world!
- *
+ * 
  */
 public class App {
-    public App(){   
-   }
-  public static void main(final String[] args){
-        System.out.println("Hello World!");
-   }
- }
+	
+	public int test1;
+	
+	private int test2;
+	
+	public App() {
+	}
+
+	public static void main(final String[] args) {
+		System.out.println("Hello World!");
+	}
+}
